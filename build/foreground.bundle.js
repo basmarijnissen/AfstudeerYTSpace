0e7405f9a83e5cafa92b9d81a5934022e695b503
basmarijnissen <62103353+basmarijnissen@users.noreply.github.com>
2021-05-26 13:09:57 +0200

    Webpack update

--- build/foreground.bundle.js
+++ build/foreground.bundle.js
@@ -1,124 +1,72 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/foreground.js");
-/******/ })
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
 /************************************************************************/
-/******/ ({
-
-/***/ "./src/js/foreground.js":
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
 /*!******************************!*\
   !*** ./src/js/foreground.js ***!
   \******************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+__webpack_require__.r(__webpack_exports__);
+Object(function webpackMissingModule() { var e = new Error("Cannot find module 'emoji-slider'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+var YTBalkrechts = document.getElementsByClassName('ytp-right-controls')[0],
+    YTBalk = document.getElementsByClassName('ytp-chrome-controls')[0],
+    icoon = document.createElement("div"),
+    popup = document.createElement("div"),
+    iconURL = chrome.extension.getURL("images/icoon.svg");
 
-var balk = document.getElementsByClassName('ytp-right-controls')[0];
-var video = document.getElementsByClassName('ytp-chrome-controls')[0];
-var blokje = document.createElement("div"),
-    popup = document.createElement("div");
-var iconURL = chrome.extension.getURL("images/icoon.svg");
-var popuptext = document.getElementsByClassName('popuptext')[0];
 
-blokje.classList.add("ytp-button");
-balk.prepend(blokje); 
-blokje.innerHTML = "<img src='" + iconURL + "'id='buttonimage'>";
 
+//voeg button class toe aan icoon 
+icoon.classList.add("ytp-button");
+//voeg icoon toe aan balk
+YTBalkrechts.prepend(icoon); 
+//plak het plaatje in icoon
+icoon.innerHTML = "<img src='" + iconURL + "'id='buttonimage'>";
+
+//voeg de popup toe aan het venster
 popup.classList.add("popup");
-video.append(popup);
-popup.innerHTML +=  "<span class='popuptext' id='myPopup'>Popup text...</span>";
+YTBalk.append(popup);
+popup.innerHTML +=  "<span class='popuptext' id='myPopup'></span>";
 
-blokje.onclick = function() { 
+//voeg de emotieslider toe aan de popup
+var popuptext = document.getElementsByClassName('popuptext')[0];
+popuptext.innerHTML += "<emoji-slider id=PleasureSlider></emoji-slider>";
+const PleasureSlider = document.querySelector('#PleasureSlider');
+
+PleasureSlider.addEventListener('change', () => {
+    const v = PleasureSlider.value;
+    if (v < 0.2) {
+        PleasureSlider.emoji = '🙁';
+    } else if (v < 0.4) {
+        PleasureSlider.emoji = '😕';
+    } else if (v < 0.6) {
+        PleasureSlider.emoji = '😐';
+    } else if (v < 0.8) {
+        PleasureSlider.emoji = '🙂';
+    } else {
+        PleasureSlider.emoji = '😀';
+    }
+  });
+
+icoon.onclick = function() { 
     var popup = document.getElementsByClassName('popup')[0];
     popup.classList.toggle("show");
     popuptext.classList.toggle('show');
-}
-
-popuptext.innerHTML += "<emoji-slider emoji='😍'></emoji-slider>";
-
-
-
-/***/ })
-
-/******/ });
+};
+/******/ })()
+;
 //# sourceMappingURL=foreground.bundle.js.map
