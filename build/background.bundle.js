742ec63b3a6d6c1f84ad8e6fe8a2a8297b4e9b40
basmarijnissen <62103353+basmarijnissen@users.noreply.github.com>
2021-05-30 20:03:08 +0200

    Data verzamelen added

--- build/background.bundle.js
+++ build/background.bundle.js
@@ -1,32 +1,308 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./node_modules/uuid/dist/esm-browser/regex.js":
+/*!*****************************************************!*\
+  !*** ./node_modules/uuid/dist/esm-browser/regex.js ***!
+  \*****************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (/^(?:[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000)$/i);
+
+/***/ }),
+
+/***/ "./node_modules/uuid/dist/esm-browser/rng.js":
+/*!***************************************************!*\
+  !*** ./node_modules/uuid/dist/esm-browser/rng.js ***!
+  \***************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ rng)
+/* harmony export */ });
+// Unique ID creation requires a high quality random # generator. In the browser we therefore
+// require the crypto API and do not support built-in fallback to lower quality random number
+// generators (like Math.random()).
+var getRandomValues;
+var rnds8 = new Uint8Array(16);
+function rng() {
+  // lazy load so that environments that need to polyfill have a chance to do so
+  if (!getRandomValues) {
+    // getRandomValues needs to be invoked in a context where "this" is a Crypto implementation. Also,
+    // find the complete implementation of crypto (msCrypto) on IE11.
+    getRandomValues = typeof crypto !== 'undefined' && crypto.getRandomValues && crypto.getRandomValues.bind(crypto) || typeof msCrypto !== 'undefined' && typeof msCrypto.getRandomValues === 'function' && msCrypto.getRandomValues.bind(msCrypto);
+
+    if (!getRandomValues) {
+      throw new Error('crypto.getRandomValues() not supported. See https://github.com/uuidjs/uuid#getrandomvalues-not-supported');
+    }
+  }
+
+  return getRandomValues(rnds8);
+}
+
+/***/ }),
+
+/***/ "./node_modules/uuid/dist/esm-browser/stringify.js":
+/*!*********************************************************!*\
+  !*** ./node_modules/uuid/dist/esm-browser/stringify.js ***!
+  \*********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _validate_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./validate.js */ "./node_modules/uuid/dist/esm-browser/validate.js");
+
+/**
+ * Convert array of 16 byte values to UUID string format of the form:
+ * XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+ */
+
+var byteToHex = [];
+
+for (var i = 0; i < 256; ++i) {
+  byteToHex.push((i + 0x100).toString(16).substr(1));
+}
+
+function stringify(arr) {
+  var offset = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+  // Note: Be careful editing this code!  It's been tuned for performance
+  // and works in ways you may not expect. See https://github.com/uuidjs/uuid/pull/434
+  var uuid = (byteToHex[arr[offset + 0]] + byteToHex[arr[offset + 1]] + byteToHex[arr[offset + 2]] + byteToHex[arr[offset + 3]] + '-' + byteToHex[arr[offset + 4]] + byteToHex[arr[offset + 5]] + '-' + byteToHex[arr[offset + 6]] + byteToHex[arr[offset + 7]] + '-' + byteToHex[arr[offset + 8]] + byteToHex[arr[offset + 9]] + '-' + byteToHex[arr[offset + 10]] + byteToHex[arr[offset + 11]] + byteToHex[arr[offset + 12]] + byteToHex[arr[offset + 13]] + byteToHex[arr[offset + 14]] + byteToHex[arr[offset + 15]]).toLowerCase(); // Consistency check for valid UUID.  If this throws, it's likely due to one
+  // of the following:
+  // - One or more input array values don't map to a hex octet (leading to
+  // "undefined" in the uuid)
+  // - Invalid input values for the RFC `version` or `variant` fields
+
+  if (!(0,_validate_js__WEBPACK_IMPORTED_MODULE_0__.default)(uuid)) {
+    throw TypeError('Stringified UUID is invalid');
+  }
+
+  return uuid;
+}
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (stringify);
+
+/***/ }),
+
+/***/ "./node_modules/uuid/dist/esm-browser/v4.js":
+/*!**************************************************!*\
+  !*** ./node_modules/uuid/dist/esm-browser/v4.js ***!
+  \**************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _rng_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./rng.js */ "./node_modules/uuid/dist/esm-browser/rng.js");
+/* harmony import */ var _stringify_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./stringify.js */ "./node_modules/uuid/dist/esm-browser/stringify.js");
+
+
+
+function v4(options, buf, offset) {
+  options = options || {};
+  var rnds = options.random || (options.rng || _rng_js__WEBPACK_IMPORTED_MODULE_0__.default)(); // Per 4.4, set bits for version and `clock_seq_hi_and_reserved`
+
+  rnds[6] = rnds[6] & 0x0f | 0x40;
+  rnds[8] = rnds[8] & 0x3f | 0x80; // Copy bytes to buffer, if provided
+
+  if (buf) {
+    offset = offset || 0;
+
+    for (var i = 0; i < 16; ++i) {
+      buf[offset + i] = rnds[i];
+    }
+
+    return buf;
+  }
+
+  return (0,_stringify_js__WEBPACK_IMPORTED_MODULE_1__.default)(rnds);
+}
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (v4);
+
+/***/ }),
+
+/***/ "./node_modules/uuid/dist/esm-browser/validate.js":
+/*!********************************************************!*\
+  !*** ./node_modules/uuid/dist/esm-browser/validate.js ***!
+  \********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _regex_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./regex.js */ "./node_modules/uuid/dist/esm-browser/regex.js");
+
+
+function validate(uuid) {
+  return typeof uuid === 'string' && _regex_js__WEBPACK_IMPORTED_MODULE_0__.default.test(uuid);
+}
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (validate);
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
 /*!******************************!*\
   !*** ./src/js/background.js ***!
   \******************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var uuid__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! uuid */ "./node_modules/uuid/dist/esm-browser/v4.js");
+
 var YouTubeWatch = 'https://www.youtube.com/watch?v=';
 var YouTubeHome = 'https://www.youtube.com/';
-var kijkurl, kijktijd, kijkdata;
+var dataID, kijktijd, start, end;
+var kijkdata = [];
+var videodata;
+var kijktVideo, dataPushed = new Boolean(false);
 
 //herlaad als de actieve pagina verandert
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
-  console.log(tab.url)
-    if(tab.url.indexOf(YouTubeWatch) !== -1){
-      // als pagina geladen is
+    start, end = 0;
 
-        chrome.tabs.executeScript(null, {file: "foreground.bundle.js"}, () => console.log('script loaded'))
+    // als de persoon een video gaat kijken:
+    if(tab.url.indexOf(YouTubeWatch) !== -1){
+      // als YouTube video geladen is, start script
+      chrome.tabs.executeScript(null, {file: "foreground.bundle.js"}, () => console.log('Foreground.js loaded'))
+        
+      // Begin cyclus
+      if (kijktVideo = false) { 
+        kijktVideo = true;        
+        LogStart();
+        // als er al een video openstond
+      } else if (kijktVideo = true) {
+        //sluit cyclus af en begin opnieuw
+        LogEnd();
+        LogStart();
+       }
+
+    } else if (tab.url === YouTubeHome) {
+      // als de geopende pagina geen YouTube video is
+      if (kijktVideo = true) {
+        LogEnd();
+        kijktVideo = false;
+      } else {
+        console.log('No previous and current video detected.')
+      }
+      }
+    } else if (changeInfo.status == 'loading') {
+      }
+  });
+
+  function LogStart() {
+    videodata = [];
+        //geef video een unieke id
+        dataID = (0,uuid__WEBPACK_IMPORTED_MODULE_0__.default)();
+        videodata.push(dataID);
+        console.log('Starting log of ' + dataID);
+
         //sla url op
         chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
-        kijkdata.push(tabs[0].url); 
+        videodata.push(tabs[0].url); 
+        console.log("video url saved: " + tabs[0].url);
+
         //sla tijd van openen op
-        var dt = new Date();
-        var DateStart = performance.now();
+        start = new Date();
+        console.log("Start time saved: " + start);
         });
-    } else if (tab.url === YouTubeHome) {
-    console.log('No video detected.')}
+  }
+
+  function LogEnd() {
+    if (videodata){
+      console.log('Ending log of ' + dataID);
+      // als de gebruiker de pagina sluit
+      end = new Date();
+      kijktijd = (end - start);
+      kijktijd = kijktijd / 1000;
+      videodata.push(kijktijd);
+      kijktVideo = false;
+      dataPushed = true;
+      // console.log('Ended log of ' + dataID + ".");
+      console.log("Log details: " + videodata);
+     if (dataPushed = true) {
+      kijkdata.push(videodata);
+      console.log('pushing log to kijkdata.');
+      console.log(kijkdata);
+     }
     }
-  })
-  ;
+    else {
+    console.log('No log to push to kijkdata.')
+   }
+  }
+
+  chrome.windows.onRemoved.addListener(function(windowId){
+    alert("!! Je planeet wordt bijgewerkt... !!");
+  });
+})();
 
 /******/ })()
 ;
--- src/js/background.js
+++ src/js/background.js
@@ -1,24 +1,90 @@
+import { v4 as uuidv4 } from 'uuid';
 var YouTubeWatch = 'https://www.youtube.com/watch?v=';
 var YouTubeHome = 'https://www.youtube.com/';
-var kijkurl, kijktijd, kijkdata;
+var dataID, kijktijd, start, end;
+var kijkdata = [];
+var videodata;
+var kijktVideo, dataPushed = new Boolean(false);
 
 //herlaad als de actieve pagina verandert
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
-  console.log(tab.url)
-    if(tab.url.indexOf(YouTubeWatch) !== -1){
-      // als pagina geladen is
+    start, end = 0;
 
-        chrome.tabs.executeScript(null, {file: "foreground.bundle.js"}, () => console.log('script loaded'))
+    // als de persoon een video gaat kijken:
+    if(tab.url.indexOf(YouTubeWatch) !== -1){
+      // als YouTube video geladen is, start script
+      chrome.tabs.executeScript(null, {file: "foreground.bundle.js"}, () => console.log('Foreground.js loaded'))
+        
+      // Begin cyclus
+      if (kijktVideo = false) { 
+        kijktVideo = true;        
+        LogStart();
+        // als er al een video openstond
+      } else if (kijktVideo = true) {
+        //sluit cyclus af en begin opnieuw
+        LogEnd();
+        LogStart();
+       }
+
+    } else if (tab.url === YouTubeHome) {
+      // als de geopende pagina geen YouTube video is
+      if (kijktVideo = true) {
+        LogEnd();
+        kijktVideo = false;
+      } else {
+        console.log('No previous and current video detected.')
+      }
+      }
+    } else if (changeInfo.status == 'loading') {
+      }
+  });
+
+  function LogStart() {
+    videodata = [];
+        //geef video een unieke id
+        dataID = uuidv4();
+        videodata.push(dataID);
+        console.log('Starting log of ' + dataID);
+
         //sla url op
         chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
-        kijkdata.push(tabs[0].url); 
+        videodata.push(tabs[0].url); 
+        console.log("video url saved: " + tabs[0].url);
+
         //sla tijd van openen op
-        var dt = new Date();
-        var DateStart = performance.now();
+        start = new Date();
+        console.log("Start time saved: " + start);
         });
-    } else if (tab.url === YouTubeHome) {
-    console.log('No video detected.')}
+  }
+
+  function LogEnd() {
+    if (videodata){
+      console.log('Ending log of ' + dataID);
+      // als de gebruiker de pagina sluit
+      end = new Date();
+      kijktijd = (end - start);
+      kijktijd = kijktijd / 1000;
+      videodata.push(kijktijd);
+      kijktVideo = false;
+      dataPushed = true;
+      // console.log('Ended log of ' + dataID + ".");
+      console.log("Log details: " + videodata);
+     if (dataPushed = true) {
+      kijkdata.push(videodata);
+      console.log('pushing log to kijkdata.');
+      console.log(kijkdata);
+     }
     }
-  })
-  ;
+    else {
+    console.log('No log to push to kijkdata.')
+   }
+  }
+
+  chrome.windows.onRemoved.addListener(function(windowId){
+    chrome.storage.sync.set({kijkdata: value}, function() {
+      console.log('Value is set to ' + value);
+    });
+    alert("!! Je planeet wordt bijgewerkt... !!");
+    
+  });
